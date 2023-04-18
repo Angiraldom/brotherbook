5fb1342538a672e52ab9b03d95c2aead6f8348ad
Johnatan R <johnatan.r1000@gmail.com>
2023-04-18 18:53:32 -0500

    Se agrega prueba de evento para el pixel.

--- src/app/landing/components/first-section/first-section.component.ts
+++ src/app/landing/components/first-section/first-section.component.ts
@@ -1,6 +1,9 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
+declare let gtag:Function;
+declare let fbq:Function;
+
 @Component({
   selector: 'app-first-section',
   templateUrl: './first-section.component.html',
@@ -15,6 +18,7 @@ export class FirstSectionComponent implements OnInit {
 
   desplace(id: string) {
     this.viewportScroller.scrollToAnchor(id);
+    fbq('track', 'Purchase', {currency: "USD", value: 30.00});
   }
 
 }
